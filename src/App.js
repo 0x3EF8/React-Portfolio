034873f4f68e06c35414c0b9975390b4a9fc2351
0x3ef8 <0x3ef8@gmail.com>
2024-01-23 04:47:50 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
+export default function App() {
+  return (
+    <div className="overflow-x-hidden">
+      <Hero />
+      <Footer />
+    </div>
+  );
+}
